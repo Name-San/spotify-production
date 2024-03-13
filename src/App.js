735cd4800c08bb8244e7b146de453d4242b635f2
Name-San <eman.jandog@gmail.com>
2024-03-13 13:08:59 +0800

    Updates

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 
 function App() {
-  const CLIENT_ID = "0b8aa71e7eaf40f3b2c384199c421a08"
+  const CLIENT_ID = "47010e28220345c18ce0ddf8b9ee9d47"
   const REDIRECT_URI = "http://localhost:3000/"
   const AUTH_ENDPOINT = "https:/accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
 
 
-  const [artist, setArtist] = useState("")
+  const [spotifydata, setSpotifyData] = useState([])
   const [searchKey, setSearchKey] = useState("")
   const [token, setToken] = useState("")
-  
+  const [tokenStatus, setTokenStatus] = useState(true)
+  const [dataType, setDataType] = useState("artist")
+
   useEffect(() => {
     
     const hash = window.location.hash
@@ -20,8 +22,6 @@ function App() {
 
     if (!token && hash) {
       token = hash.substring(1).split('&').find(elem => elem.startsWith("access_token")).split("=")[1]
-      //token = 'BQBCj9LehjUoJBaNPGF3OAMmh-_bApSauqiWBFM05Xb6mdVPHluuoYxAEjtV3vRtLci61a8dd9-ABu-VrAnS74HsCEYhJPt0WVArFZQ3BERa4fgt1LvwGosFdHt3mJcdtwcbkjdlHGfZmq20KU-6TL5ikqQVUgg54QLVpqgUgOHv35sbffeRQPZOAEzoBzR1_fg'
-      console.log(token)
       window.location.hash = ""
       window.localStorage.setItem("token", token)
     }
@@ -30,54 +30,74 @@ function App() {
   }, [])
 
   const logout = () => {
-    setToken("")
+    setToken("");
     window.localStorage.removeItem("token")
   }
 
-  const searchArtists = async (e) => {
-    e.preventDefault();
+  const search = async (e) => {
+    e.preventDefault();   
     try {
-      const artistData = await fetchArtistData(searchKey, token);
-      console.log(`Artist data: ${artistData}`)
-      
-      
+      const datafetch = await fetchData(searchKey, token, dataType);
+      const type = `${dataType}s`;
+      setSpotifyData(datafetch[type].items);
+        
     } catch (error) {
-      console.error('Error searching for aritst:', error.message);
-      alert('Error searching for track. Please try again.');
+      console.error('Error searching for artist:');
     }
   }
 
-  const fetchArtistData = async (searchKey, token) => {
-    const apiURL = `https://api.spotify.com/v1/search?q=${searchKey}&type=artist`
-
+  const fetchData = async (searchKey, token, type) => {
+    const apiURL = `https://api.spotify.com/v1/search?q=${searchKey}&type=${type}`
+    console.log(apiURL);
     const response = await fetch(apiURL, {
       headers: {
         Authorization: `Bearer ${token}`
       }
     });
+    handleToken(response.status);
 
     if (!response.ok) {
-      throw new Error('Failed to fetch artist data');
+      throw new Error(`Failed to fetch ${type} data`);
     }
     return await response.json();
+  }
+
+  const handleToken = async (status) => {
+    if (status === 401) {
+      setTokenStatus(false);
+      throw new Error('Token expired');
+    } else {
+      setTokenStatus(true);
+    }
+  }
+
+  const renderFetchData = () => {
+    return spotifydata.map(d => (
+        <div key={d.id}>
+          {d.images.length ? <img width={"100%"} src={d.images[0].url} alt=""/> : <div>No Image</div>}
+          {d.name}
+        </div>
+      )) 
   }
 
   return (
     <div className="App"> 
       <header className="App-header">
         <h1>Spotify Production</h1>
-        {!token ? <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
-          : <button onClick={logout}>Logout</button>
-        }
-        {token ?
-          <form onSubmit={searchArtists}>
+        {token ? 
+          <form onSubmit={search}>
             <input type="text" onChange={e => setSearchKey(e.target.value)}></input>
-            <button type={"submit"}>Search</button>
+            <button type={"submit"}>Search</button>            
           </form>
-          : <h2>Please login</h2>
-
+          : <h4>Authorize your spotify account</h4>
         }
+        {!token ? <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+          : <button className="btn" onClick={logout}>Logout</button>
+        }
+        {!tokenStatus ? <h2>Please re-authorize your spotify account</h2> : <></>}
 
+        {renderFetchData()}
+        
       </header>
     </div>
   );
